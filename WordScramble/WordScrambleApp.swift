95380a695f39bab583349dd3ae574ed42016ab63
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-04-12 07:46:28 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by sanmi_personal on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
